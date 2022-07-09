e2afc7ef5d083e33daf797e85e066c38359a7170
luxDuck <108199623+luxDuck@users.noreply.github.com>
2022-07-09 06:32:54 -0700

    Update turrets.js

--- scripts/turrets.js
+++ scripts/turrets.js
@@ -1,39 +1,3 @@
-// effects
-
-const redBlast = new Effect(40, 100, e => {
-    Draw.color(Color.valueOf("e56666"));
-    Lines.stroke(e.fout() * 2);
-    Lines.circle(e.x, e.y, e.finpow() * 20);
-
-    Draw.color(Color.valueOf("e56666"));
-    for(let i = 0; i < 4; i++){
-        Drawf.tri(e.x, e.y, 6, 10 * e.fout(), i*90+45);
-    }
-
-    Draw.color();
-    for(let i = 0; i < 4; i++){
-        Drawf.tri(e.x, e.y, 3, 3 * e.fout(), i*90+45);
-    }
-});
-
-const purpleBlast = new Effect(40, 100, e => {
-    Draw.color(Color.valueOf("985bb0"));
-    Lines.stroke(e.fout() * 2);
-    Lines.circle(e.x, e.y, e.finpow() * 20);
-
-    Draw.color(Color.valueOf("985bb0"));
-    for(let i = 0; i < 4; i++){
-        Drawf.tri(e.x, e.y, 6, 10 * e.fout(), i*90+45);
-    }
-
-    Draw.color();
-    for(let i = 0; i < 4; i++){
-        Drawf.tri(e.x, e.y, 3, 3 * e.fout(), i*90+45);
-    }
-});
-
-// end effects
-
 // luxDuck's content
 
 // rip
